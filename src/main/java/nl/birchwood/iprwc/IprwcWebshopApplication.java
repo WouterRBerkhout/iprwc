e950050d71567feb74ed4319d518dd17ffc86c4f
Wouter Berkhout <s1125948@student.hsleiden.nl>
2022-01-17 13:35:01 +0100

    :tada: Initial commit

---
+++ src/main/java/nl/birchwood/iprwc/IprwcWebshopApplication.java
@@ -0,0 +1,13 @@
+package nl.birchwood.iprwc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IprwcWebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IprwcWebshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/birchwood/iprwc/IprwcWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.birchwood.iprwc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IprwcWebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
